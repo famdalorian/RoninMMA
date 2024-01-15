a4f53ab93847b5bf947e7c86ed049bf90de3a81a
Jordan W <famdalorian@proton.me>
2024-01-15 17:02:33 +0000

    classes images added

--- ronin-frontend/src/Pages/Classes.jsx
+++ ronin-frontend/src/Pages/Classes.jsx
@@ -7,7 +7,9 @@ function Classes() {
     <div className="classes-container">
       <h1>Classes Information</h1>
       <section className="class-section">
-        <h2>Boxing</h2>
+        <h2>Boxing</h2>   
+           <h1 className="class-boxing"></h1>
+
         <p>
           Our boxing classes focus on developing fundamental boxing techniques, footwork, and
           conditioning. Suitable for all levels, from beginners to advanced.
@@ -16,6 +18,7 @@ function Classes() {
 
       <section className="class-section">
         <h2>MMA (Mixed Martial Arts)</h2>
+        <div className="class-mma"></div>
         <p>
           Join our MMA classes to learn a combination of striking, wrestling, and grappling
           techniques. Open to all skill levels, our instructors provide personalized training for
@@ -25,6 +28,7 @@ function Classes() {
 
       <section className="class-section">
         <h2>Muay Thai</h2>
+        <div className="class-thai"></div>
         <p>
           Experience the art of Muay Thai with our dedicated classes. Learn traditional Muay Thai
           techniques, including powerful strikes and clinch work. Suitable for all levels of
@@ -34,6 +38,8 @@ function Classes() {
 
       <section className="class-section">
         <h2>Wrestling</h2>
+        <div className="class-wrestling"></div>
+
         <p>
           Our wrestling classes cover takedowns, ground control, and defensive strategies. Whether
           you're a beginner or an experienced wrestler, our classes cater to all levels.
@@ -42,6 +48,7 @@ function Classes() {
 
       <section className="class-section">
         <h2>BJJ (Brazilian Jiu-Jitsu)</h2>
+        <div className="class-bjj"></div>
         <p>
           Join our BJJ classes to explore ground fighting, submissions, and positional control. Our
           instructors ensure a welcoming environment for practitioners of all levels.
@@ -50,6 +57,8 @@ function Classes() {
 
       <section className="class-section">
         <h2>Strength and Conditioning</h2>
+        <div className="class-gym"></div>
+
         <p>
           Improve your overall fitness with our strength and conditioning classes. Tailored for all
           fitness levels, these classes focus on building strength, endurance, and flexibility.
